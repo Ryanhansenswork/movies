6f8eebdc6f298a1f1799da96cd958d2b2a76d9dd
Ryanhansenswork <ryanhanseng@gmail.com>
2024-12-18 09:44:44 -0500

    MongoDB

--- client/js/write-data.js
+++ client/js/write-data.js
@@ -1,46 +1,38 @@
-document.getElementById("submit").addEventListener("click", function() 
-{
-    //alert("Submit button was pressed"); 
+
+document.getElementById("submit").addEventListener("click", function() {
     let movieTitle = $('#movieTitle').val();
     let movieDirector = $('#movieDirector').val();
     let movieGenre = $('#movieGenre').val();
     let movieYear = $('#movieYear').val();
     let movieRating = $('#movieRating').val();
 
-    let jsonString = {movieTitle: movieTitle, 
-        movieDirector: movieDirector, 
-        movieGenre: movieGenre, 
-        movieYear:movieYear, 
-        movieRating:movieRating};
-        console.log(JSON.stringify(jsonString))
+  
+    let jsonString = {
+        movieTitle: movieTitle,
+        movieDirector: movieDirector,
+        movieGenre: movieGenre,
+        movieYear: movieYear,
+        movieRating: movieRating
+    };
+
+    console.log("Sending data:", JSON.stringify(jsonString));
+
     $.ajax({
-        url: moviesURL = "/write-record",
-        type: "post",
-        data: jsonString,
-        success: function(response)
-        {
-            let data = JSON.parse(response);
-            if (data.msg = "SUCCESS")
-            {
-                alert("data saved")
-            }
-            else
-            {
-                console.log(data.msg)
+        url: "/write-record",
+        type: "POST",
+        contentType: "application/json",  
+        data: JSON.stringify(jsonString), 
+        dataType: "json",  
+        success: function(response) {
+            if (response.msg === "SUCCESS") {
+                alert("Data saved successfully!");
+            } else {
+                console.log("Error message:", response.msg);
             }
         },
-        error: function(err)
-        {
-            console.log(err)
+        error: function(err) {
+            console.log("Error:", err);
         }
-    })
     });
-
-document.getElementById("clear").addEventListener("click", function() 
-{
-    document.getElementById("movieTitle").value = "";
-    document.getElementById("movieDirector").value = "";
-    document.getElementById("movieGenre").value = "";
-    document.getElementById("movieYear").value = "";
-    document.getElementById("movieRating").value = "";
 });
+
--- server/services.js
+++ server/services.js
@@ -1,101 +1,73 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const app = express();
 
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
 
 app.use(express.json()); 
 
-const DB_FILE = path.join(__dirname, './files/data.txt');
 
-let services = function(app) {
+const MONGO_URL = "mongodb://localhost:27017";
+const DATABASE_NAME = "movieArchive"; 
+const COLLECTION_NAME = "movies"; 
 
-   app.delete('/delete-record', function (req, res) {
-    const idToDelete = req.body.id;
-    
-    if (!fs.existsSync(DB_FILE)) {
-        return res.send(JSON.stringify({ msg: "FILE NOT FOUND" }));
-    }
-
-    fs.readFile(DB_FILE, "utf8", function (err, data) {
-        if (err) {
-            return res.send(JSON.stringify({ msg: err }));
-        }
-
-        let movieData = JSON.parse(data);
-        const updatedMovies = movieData.filter(movie => movie.id !== idToDelete);
-
-        fs.writeFile(DB_FILE, JSON.stringify(updatedMovies), function (err) {
-            if (err) {
-                return res.send(JSON.stringify({ msg: err }));
-            }
-
-            res.send(JSON.stringify({ msg: "SUCCESS" }));
-        });
+let db;
+MongoClient.connect(MONGO_URL)
+    .then(client => {
+        console.log("Connected to MongoDB");
+        db = client.db(DATABASE_NAME);
+    })
+    .catch(err => {
+        console.error("Failed to connect to MongoDB:", err);
     });
-});
 
-   app.get("/read-data", (req, res) => {
-    const filePath = path.join(__dirname, "./files/data.txt");
+let services = function (app) {
+    
+    
+    app.delete('/delete-record', async (req, res) => {
+        const idToDelete = req.body.id;
 
-    fs.readFile(filePath, "utf-8", (err, data) => {
-        if (err) {
-            console.error("Error reading data.txt:", err);
+        try {
+            const result = await db.collection(COLLECTION_NAME).deleteOne({ id: idToDelete });
+            if (result.deletedCount > 0) {
+                res.json({ msg: "SUCCESS" });
+            } else {
+                res.status(404).json({ msg: "NOT FOUND" });
+            }
+        } catch (err) {
+            console.error("Error deleting record:", err);
+            res.status(500).json({ msg: "Error deleting record" });
+        }
+    });
+
+    app.get("/read-data", async (req, res) => {
+        try {
+            const movies = await db.collection(COLLECTION_NAME).find().toArray();
+            res.status(200).json(movies);
+        } catch (err) {
+            console.error("Error reading data:", err);
             res.status(500).json({ msg: "Error reading data" });
-        } else {
-            try {
-                const movies = JSON.parse(data);
-                res.status(200).json(movies);
-            } catch (parseError) {
-                console.error("Error parsing data:", parseError);
-                res.status(500).json({ msg: "Error parsing data" });
-            }
         }
     });
-});
+
     
-    
-    app.post('/write-record', function(req, res) {
+    app.post('/write-record', async (req, res) => {
         let id = "mov" + Date.now();
 
         let data = {
             id: id,
             movieTitle: req.body.movieTitle,
             movieDirector: req.body.movieDirector,
-            movieGenre: req.body.moviegenre,
+            movieGenre: req.body.movieGenre,
             movieYear: req.body.movieYear,
             movieRating: req.body.movieRating
         };
-        
-        let movieData = [];
-        if (fs.existsSync(DB_FILE)) {
-            fs.readFile(DB_FILE, "utf8", function(err, fileData) {
-                if (err) {
-                    res.send(JSON.stringify({ msg: err }));
-                } else {
-                    movieData = JSON.parse(fileData);
-                    movieData.push(data);
 
-                    fs.writeFile(DB_FILE, JSON.stringify(movieData), function(err) {
-                        if (err) {
-                            res.send(JSON.stringify({ msg: err }));
-                        } else {
-                            res.send(JSON.stringify({ msg: "SUCCESS" }));
-                        }
-                    });
-                }
-            });
-        } else {
-            movieData.push(data);
-            console.log(JSON.stringify(data));
-            
-            fs.writeFile(DB_FILE, JSON.stringify(movieData), function(err) {
-                if (err) {
-                    res.send(JSON.stringify({ msg: err }));
-                } else {
-                    res.send(JSON.stringify({ msg: "SUCCESS" }));
-                }
-            });
+        try {
+            await db.collection(COLLECTION_NAME).insertOne(data);
+            res.json({ msg: "SUCCESS" });
+        } catch (err) {
+            console.error("Error writing record:", err);
+            res.status(500).json({ msg: "Error writing record" });
         }
     });
 };
